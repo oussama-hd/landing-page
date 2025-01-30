bf74a0798511015ecf0a93f91f26ebaf93ae7d46
oussama mohammadi <oussama.mohammadi19@gmail.com>
2025-01-30 09:07:14 +0100

    add contact-us section

---
+++ src/app/pages/main-page/components/contact-us/contact-us.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'app-contact-us',
+  templateUrl: './contact-us.component.html',
+  styleUrl: './contact-us.component.scss'
+})
+export class ContactUsComponent {
+
+}
